499fdaba64158bfb57061dc40a74056721330eec
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2022-03-13 14:57:05 +0900

    fix: bug about return type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { useRef } from 'react';
 
+// extract await response type of Promise
+type AwaitType<T extends Promise<any>> = T extends Promise<infer R> ? R : never;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useMCP = <F extends (...args: any[]) => Promise<any>>(f: F) => {
   const isProcessing = useRef(false);
 
   const multipleClickPreventer = async (
     ...args: Parameters<F>
-  ): Promise<ReturnType<F> | void> => {
+  ): Promise<AwaitType<ReturnType<F>> | void> => {
     if (isProcessing.current) {
       return;
     }
